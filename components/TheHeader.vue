9e9a007c174fdedae5782be35a852c27e77a4718
Alvaro <alvaro.saburido@gmail.com>
2022-05-26 10:46:26 +0200

    feat: header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="">
+    <div class="container mx-auto flex">
+      <div class="flex-1">
+        <a
+          href="/"
+          class="logo flex items-center font-display font-extrabold text-gray-800"
+        >
+          <img src="/logo.svg" class="h-10 w-10 mr-2" alt="Sushi Wuut" />
+          Sushi<span class="text-shrimp-400">Wuut</span>
+        </a>
+      </div>
+      <div class="flex-1 text-right"></div>
+    </div>
+  </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@unocss/nuxt'],
+  css: ['@unocss/reset/tailwind.css'],
   unocss: {
     // presets
     uno: true, // enabled `@unocss/preset-uno`
